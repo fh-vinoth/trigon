56243dcffc182c71fb44619297ce8b66cece8b73
kumarappan-t2s <kumarappan.s@foodhub.com>
2022-04-20 17:18:17 +0530

    v2.7 : Saving Failures for Retried Tests - Suite & class file's changes for testing the functionality implementation

--- src/test/java/com/fh/unittests/frameworktest/frameworkTest2.java
+++ src/test/java/com/fh/unittests/frameworktest/frameworkTest2.java
@@ -2,17 +2,30 @@ package com.fh.unittests.frameworktest;
 
 import com.fh.core.TestLocalController;
 
+import com.trigon.wrapper.TestModels;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class frameworkTest2 extends TestLocalController {
 
     @Test
-    public void unitTest() {
+    public void trailTest() {
 //        browser().get("https://www.amazon.com/");
-        browser().findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
+//        browser().findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
 //        browser().quit();
-        author_ScenarioName("bhaskar_parallelClass1_Method3", "parallelClass1_Method3 scenario");
-        logReport("FAIL", "parallelClass1_Method3 PASSED");
+        try {
+            author_ScenarioName("bhaskar_parallelClass1_Method3", "parallelClass1_Method3 scenario");
+            logReport("FAIL", "parallelClass1_Method3 PASSED");
+            TestModels testModels = new TestModels();
+            testModels.verifyTitle("Amazon.com. Spend less. Smile more.","Exact");
+            testModels.verifyTitle("Google","Exact");
+        }
+        catch (Exception e)
+        {
+            hardFail(e);
+        }
+        finally {
+            testTearDown();
+        }
     }
 }
