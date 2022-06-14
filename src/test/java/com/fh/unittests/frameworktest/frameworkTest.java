643d549d95b5bdb067271e8d6df727bd3ae7b2b8
fh-vikram <vikram.s@foodhub.com>
2022-06-14 13:26:21 +0530

    v2.7 : Removing additional pushed files

--- src/test/java/com/fh/unittests/frameworktest/frameworkTest.java
+++ src/test/java/com/fh/unittests/frameworktest/frameworkTest.java
@@ -1,82 +1,31 @@
 package com.fh.unittests.frameworktest;
 
 
-import com.fh.core.LocalAPIController;
 import com.fh.core.TestLocalController;
 
-import com.trigon.annotations.ExcelSheet;
-import com.trigon.dataprovider.DataProviders;
-import com.trigon.wrapper.TestModels;
-import org.apache.logging.log4j.core.config.Order;
 import org.openqa.selenium.By;
-import org.testng.ITestResult;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 
 public class frameworkTest extends TestLocalController {
 
-    @Test
+    @Test(enabled = false)
     public void unitTest() {
-
+        browser().findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
         author_ScenarioName("bhaskar_parallelClass1_Method1", "parallelClass1_Method1 scenario");
-//        browser().findElement(By.id("twotabsearchtextbox1")).sendKeys("iphone");
-      try {
-          TestModels testModels = new TestModels();
-//          testModels.navigateToUrl("https://www.google.com");
-//          logReport("FAIL", "parallelClass1_Method1 PASSED");
-//          Scanner sc=new Scanner(System.in);
-//          System.out.println("Enter zxc for passing");
-//        String test= tEnv().getApiEmailID();
-//         testModels.compareText(test,"teamautomation17@gmail.com","Exact");
-          testModels.verifyTitle("Amazon.com. Spend less. Smile more.","Exact");
-          hardWait(500);
-        testModels.verifyTitle("Google","Exact");
-//        System.out.println(testModels.getCurrentPageURL());--
-//         testModels.compareText(tEnv().getJsonFilePath(),"2","Exact");
-
-      }
-      catch(Exception e){
-          hardFail(e);
-
-      }
-      finally {
-//          tEnv().setJsonFilePath("2");
-//          tEnv().setApiEmailID("teamautomation17@gmail.com");
-          testTearDown();
-      }
+        logReport("PASS", "parallelClass1_Method1 PASSED");
     }
 
-//    @Test
-//    public void testGoogleSearch() {
-////        WebElement element = browser().findElement(By.xpath("(//*[@type=\"text2\"])[1]"));
-////        element.sendKeys("iPhone");
-//        //element.submit();
-//        //browser().findElement(By.linkText("Web")).click();
-//        //tObj().frameworkTest().searchTextBox_enterText("bhaskar");
-//
-//        author_ScenarioName("bhaskar_parallelClass1_Method2", "parallelClass1_Method2 scenario");
-//        logReport("PASS", "parallelClass1_Method2 PASSED");
-//        try {
-//            TestModels testModels = new TestModels();
-//            testModels.navigateToUrl("https://www.flipkart.com/");
-//            hardWait(1000);
-//            testModels.verifyTitle("Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!","Exact");
-//            testModels.verifyTitle("Google","Exact");
-//
-//        }
-//        catch(Exception e){
-//            hardFail(e);
-//
-//        }
-//        finally {
-////          tEnv().setJsonFilePath("2");
-////          tEnv().setApiEmailID("teamautomation17@gmail.com");
-//            testTearDown();
-//        }
-//
-//    }
+    @Test
+    public void testGoogleSearch() {
+//        WebElement element = browser().findElement(By.xpath("(//*[@type=\"text2\"])[1]"));
+//        element.sendKeys("iPhone");
+        //element.submit();
+        //browser().findElement(By.linkText("Web")).click();
+        //tObj().frameworkTest().searchTextBox_enterText("bhaskar");
+        author_ScenarioName("bhaskar_parallelClass1_Method2", "parallelClass1_Method2 scenario");
+        logReport("PASS", "parallelClass1_Method2 PASSED");
+
+    }
 }
--- src/test/java/com/fh/unittests/frameworktest/frameworkTest2.java
+++ src/test/java/com/fh/unittests/frameworktest/frameworkTest2.java
@@ -2,30 +2,17 @@ package com.fh.unittests.frameworktest;
 
 import com.fh.core.TestLocalController;
 
-import com.trigon.wrapper.TestModels;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class frameworkTest2 extends TestLocalController {
 
     @Test
-    public void trailTest() {
+    public void unitTest() {
 //        browser().get("https://www.amazon.com/");
-//        browser().findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
+        browser().findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
 //        browser().quit();
-        try {
-            author_ScenarioName("bhaskar_parallelClass1_Method3", "parallelClass1_Method3 scenario");
-            logReport("FAIL", "parallelClass1_Method3 PASSED");
-            TestModels testModels = new TestModels();
-            testModels.verifyTitle("Amazon.com. Spend less. Smile more.","Exact");
-            testModels.verifyTitle("Google","Exact");
-        }
-        catch (Exception e)
-        {
-            hardFail(e);
-        }
-        finally {
-            testTearDown();
-        }
+        author_ScenarioName("bhaskar_parallelClass1_Method3", "parallelClass1_Method3 scenario");
+        logReport("FAIL", "parallelClass1_Method3 PASSED");
     }
 }
