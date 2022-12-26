62cdf0138afd41419cd2ced5974a7d2518c366ca
Nisha <nisha.achuthan@foodhub.com>
2022-12-27 02:02:09 +0530

    UpdateTestRailTestStatus class creation

--- src/main/java/com/trigon/testrail/TestRailJsonCreationNewMap.java
+++ src/main/java/com/trigon/testrail/TestRailJsonCreationNewMap.java
@@ -24,12 +24,21 @@ import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
-public class TestRailJsonCreationNewMap extends IOException{
+public class TestRailJsonCreationNewMap extends IOException {
     public static JsonWriter writer;
     HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>> classNameArray = new HashMap<>();
-    HashMap<String,String> passedList = new HashMap<>();
-    HashMap<String,String> failedList = new HashMap<>();
-    HashMap<String,String> skippedList = new HashMap<>();
+    HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>> testInfo = new HashMap<>();
+    HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>> testIdInfo = new HashMap<>();
+    HashMap<String, HashMap<String, String>> passedFailed = new HashMap<>();
+    HashMap<String, String> passedList = new HashMap<>();
+    HashMap<String, String> failedList = new HashMap<>();
+    HashMap<String, String> skippedList = new HashMap<>();
+
+    int count = 1;
+   /* HashMap<String, String> passedList = new HashMap<>();
+    HashMap<String, String> failedList = new HashMap<>();
+    HashMap<String, String> skippedList = new HashMap<>();*/
+
     static {
         try {
             writer = new JsonWriter(new BufferedWriter(new FileWriter("src/test/resources/Configuration/TestCaseStatusAfterExeNew1.json")));
@@ -44,6 +53,7 @@ public class TestRailJsonCreationNewMap extends IOException{
 
     @Test
     public void getTestRail(Method m) throws IOException {
+
        /* HashMap<String, String> passed = new HashMap<>();
         passed.put("Coupon_" + getClass().getSimpleName(), "C1234454");
         passed.put("Coupon_" + getClass().getSimpleName(), "C1234455");
@@ -58,45 +68,74 @@ public class TestRailJsonCreationNewMap extends IOException{
         skipped.put("Statistics_" + getClass().getSimpleName(), "C1234452");
         HashMap<String, String> failedReason = new HashMap<>();
         failedReason.put("C1234452", "401 Issue");*/
-        setJson("C1234452","Get statistics1","Passed",getClass().getSimpleName(),m.getName());
-        setJson("C1234453","Get statistics2","Passed",getClass().getSimpleName(),m.getName());
-        setJson("C1234454","Get statistics3","Failed",getClass().getSimpleName(),m.getName());
-        setJson("C1234455","Get order1","Failed","OrderPlacement","OrderPlacement1");
-        setJson("C1234456","Get order2","Passed","OrderPlacement","OrderPlacement2");
-        setJson("C1234457","Get driver","Skipped","Driver","DriverStatus");
+
+        /*testInfo = setJson("C1234452", "Get statistics and validate for delivery", "Passed", getClass().getSimpleName(), m.getName());
+        testInfo.putAll(setJson("C1234453", "Get statistics and validate for collection", "Passed", getClass().getSimpleName(), m.getName()));
+        testInfo.putAll(setJson("C1234453", "Get statistics and validate for instore", "Skipped", getClass().getSimpleName(), m.getName()));
+        testInfo.putAll(setJson("C1234454", "502 Bad Gateway Error", "Failed", getClass().getSimpleName(), m.getName()));
+        testInfo.putAll(setJson("C1234455", "401 Unauthorized Exception", "Failed", "OrderPlacement", "OrderPlacement1"));
+        testInfo.putAll(setJson("C1234456", "Order Placement validation for collection", "Passed", "OrderPlacement", "OrderPlacement2"));
+        testInfo.putAll(setJson("C1234457", "Get driver", "Skipped", "Driver", "DriverStatus"));
+*/
+        /*testInfo = setJson("C1234452", "Get statistics1", "Passed", getClass().getSimpleName(), m.getName());
+        addToMap(testInfo);
+        testInfo = setJson("C1234453", "Get statistics2", "Passed", getClass().getSimpleName(), m.getName());
+        addToMap(testInfo);
+        testInfo = setJson("C1234454", "Get statistics3", "Failed", getClass().getSimpleName(), m.getName());
+        addToMap(testInfo);
+        testInfo = setJson("C1234455", "Get order1", "Failed", "OrderPlacement", "OrderPlacement1");
+        addToMap(testInfo);
+        testInfo = setJson("C1234456", "Get order2", "Passed", "OrderPlacement", "OrderPlacement2");
+        addToMap(testInfo);
+        testInfo = setJson("C1234457", "Get driver", "Skipped", "Driver", "DriverStatus");
+        addToMap(testInfo);*/
+        setJson("C1234452", "Get statistics1", "Passed", getClass().getSimpleName(), m.getName());
+        setJson("C1234453", "Get statistics2", "Passed", getClass().getSimpleName(), m.getName());
+        setJson("C1234454", "Get statistics3", "Failed", getClass().getSimpleName(), m.getName());
+        setJson("C1234455", "Get order1", "Failed", "OrderPlacement", "OrderPlacement1");
+        setJson("C1234456", "Get order2", "Passed", "OrderPlacement", "OrderPlacement2");
+        setJson("C1234457", "Get driver", "Skipped", "Driver", "DriverStatus");
         generateJsonFile(classNameArray);
-        updateTestRailStatus();
     }
 
-    public void setJson(String testCaseIds,String reasonOrScenario,String testStatus, String className, String methodName) {
+    public HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>> setJson(String testCaseIds, String reasonOrScenario, String testStatus, String className, String methodName) {
 
-            if(classNameArray.containsKey(testCaseIds)){
-
-            }
-            if(testStatus.equalsIgnoreCase("Passed")){
-                passedList.put(testCaseIds,reasonOrScenario);
-            }
-            else if(testStatus.equalsIgnoreCase("Failed")){
-                failedList.put(testCaseIds,reasonOrScenario);
-            }else{
-                skippedList.put(testCaseIds,reasonOrScenario);
-            }
+        if (testStatus.equalsIgnoreCase("Passed")) {
+            passedList.put(testCaseIds, reasonOrScenario);
+        } else if (testStatus.equalsIgnoreCase("Failed")) {
+            failedList.put(testCaseIds, reasonOrScenario);
+        } else {
+            skippedList.put(testCaseIds, reasonOrScenario);
+        }
 
 
-        HashMap<String, HashMap<String,String>> passedFailed = new HashMap<>();
-        passedFailed.put("Passed",passedList);
-        passedFailed.put("Failed",failedList);
-        passedFailed.put("Skipped",skippedList);
+        // HashMap<String, HashMap<String, String>> passedFailed = new HashMap<>();
+       /* if (classNameArray.containsKey(className)) {
+            passedFailed.put("Passed", passedList);
+            passedFailed.put("Failed", failedList);
+            passedFailed.put("Skipped", skippedList);
+        } else {
+            passedFailed = new HashMap<>();
+            passedFailed.put("Passed", passedList);
+            passedFailed.put("Failed", failedList);
+            passedFailed.put("Skipped", skippedList);
+        }*/
+        passedFailed.put("Passed", passedList);
+        passedFailed.put("Failed", failedList);
+        passedFailed.put("Skipped", skippedList);
 
-        HashMap<String,HashMap<String, HashMap<String,String>>> methodNames = new HashMap<>();
-        methodNames.put(methodName,passedFailed);
+        HashMap<String, HashMap<String, HashMap<String, String>>> methodNames = new HashMap<>();
+        methodNames.put(methodName, passedFailed);
 
-        classNameArray.put(className,methodNames);
+        classNameArray.put(className, methodNames);
         System.out.println(classNameArray);
 
+        return classNameArray;
+
+
     }
 
-    public void generateJsonFile( HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>> json){
+    public void generateJsonFile(HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>> json) {
         Gson gson = new Gson();
         String val = gson.toJson(json, HashMap.class);
         try {
@@ -107,34 +146,11 @@ public class TestRailJsonCreationNewMap extends IOException{
             e.printStackTrace();
         }
     }
-    public void updateTestRailStatus() {
-        ObjectMapper mapper = new ObjectMapper();
-        try{
-            File fileObj = new File("src/test/resources/Configuration/TestCaseStatusAfterExeNew1.json");
-            String result = new String(Files.readAllBytes(Paths.get(String.valueOf(fileObj))));
-            Map<String, Object> map = new Gson().fromJson(result, new TypeToken<HashMap<String, Object>>() {
-            }.getType());
-            List<String> a = new ArrayList<>();
-            map.forEach((k,v)->{
-                if(k.endsWith("].passed")){
-                    System.out.println(v);
-                    a.add(String.valueOf(v));
-                }
-            });
-            for(Map.Entry<String,Object> ap : map.entrySet()){
-                String k = ap.getKey();
-                Object v = ap.getValue();
-                System.out.println(k+", "+v);
-            }
-            System.out.println(a);
-           /* Map<String, Object> testCaseId = mapper.readValue(
-                    fileObj, new TypeReference<Map<String, Object>>() {
-                    });*/
-           /* System.out.println(userData.get("API"));
-            System.out.println(userData.get("MYT"));*/
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+
+    public void addToMap(HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>> testInfo) {
+        testInfo.forEach((k, v) -> {
+            testIdInfo.put(k, v);
+        });
 
     }
 
