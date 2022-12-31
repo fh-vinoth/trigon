fe39169766fdc33c5b74ba4aba9f4f1bbf282800
Nisha <nisha.achuthan@foodhub.com>
2022-12-31 13:10:19 +0530

    Test report creation and attach to the test result path and email report

---
+++ src/main/java/com/trigon/reports/TestRailReport.java
@@ -0,0 +1,104 @@
+package com.trigon.reports;
+
+import org.testng.ITestContext;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static com.trigon.testbase.TestInitialization.trigonPaths;
+
+public class TestRailReport extends Initializers {
+
+
+
+    public void initTestRailReport() {
+        try {
+            String htmlFile = trigonPaths.getTestResultsPath() + "/TestRailReport.html";
+            String reportTask = "NA";
+            if (tEnv() != null) {
+                reportTask = tEnv().getCurrentTestMethodName();
+            }
+            if (!new File(htmlFile).exists()) {
+                BufferedWriter htmlWriter = new BufferedWriter(new FileWriter(htmlFile));
+                int logoHeaderLength = 2;
+                System.out.println(logoHeaderLength);
+                htmlWriter.write("<!DOCTYPE html>\n" +
+                        "<html lang=\"en\">\n" +
+                        "<head>\n" +
+                        "    <meta charset=\"UTF-8\">\n" +
+                        "    <title>FoodHub Custom Email Report</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "<table style=\"width: 90%;font-family: Roboto,sans-serif;font-size: 13px;color: #555555;text-align: center;border-radius: 20px;margin-left:auto;margin-right:auto;overflow: hidden;border-collapse: collapse;background-color: #f5f2f2\">\n" +
+                        "    <tbody>\n" +
+                        "    <th style=\"padding-left: 30px;text-align: left;background: #e0dbdb;height: 60px;\"><img alt=\"FoodHub\" height=\"20\" src=\"https://s3.amazonaws.com/t2s-staging-automation/Docs/foodhub_email_logo.png\" width=\"114\"></th>\n" +
+                        "    <th colspan=\""+logoHeaderLength+"\" style=\"text-align: center;background: #e0dbdb;height: 60px;\"> Task Name : "+reportTask+"</th>\n" +
+                        "    <th style=\"text-align: left;background: #e0dbdb;height: 60px;\">\n" +
+                        "        <div>Executed By : "+System.getProperty("user.name")+"</div>\n" +
+                        "        <div>Executed OS : "+System.getProperty("os.name") +"</div>\n" +
+                        "        <div>TestRail Link : <a href =\"https://www.testrail.com\"> Navigate to TestRail</a></div>\n" +
+                        "    </th>\n" +
+                        "<style> td {\n" +
+                        "  border: 1px solid black;\n" +
+                        "  border-color: #e0e4e4;\n" +
+                        "} \n"+
+                        "</style>\n"+
+                        "    <tr style=\"background: #797575;height: 40px; font-weight: bold;color: #faf8f8\">\n");
+
+
+                    htmlWriter.write("<td> ClassName </td>\n");
+                    htmlWriter.write("<td> Passed </td>\n");
+                    htmlWriter.write("<td> Failed </td>\n");
+                    htmlWriter.write("<td> Skipped </td>\n");
+                htmlWriter.write("                    </tr>\n");
+                htmlWriter.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void addRowToTestRailReport(String... values) {
+        try {
+            if (values.length > 0) {
+                String htmlFile = trigonPaths.getTestResultsPath() + "/TestRailReport.html";
+
+                BufferedWriter htmlWriter = new BufferedWriter(new FileWriter(htmlFile, true));
+                htmlWriter.write(" <tr style=\"height: 40px;\">\n");
+                for (int i = 0; i < values.length; i++) {
+                    htmlWriter.write("<td> " + values[i] + "</td>\n");
+                }
+                htmlWriter.write("                    </tr>\n");
+                htmlWriter.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void tearDownCustomReport(ITestContext iTestContext) {
+        String htmlFile = trigonPaths.getTestResultsPath() + "/TestRailReport.html";
+        if (new File(htmlFile).exists()) {
+            try {
+                BufferedWriter htmlWriter = new BufferedWriter(new FileWriter(htmlFile, true));
+                htmlWriter.write("    </tbody>\n" +
+                        "</table>\n" +
+                        "</body>\n" +
+                        "</html>");
+                htmlWriter.close();
+                /*JsonWriter writer = new JsonWriter(new BufferedWriter(new FileWriter(trigonPaths.getSupportFileHTMLPath() + "/" + "CustomReport.json")));
+                writer.beginObject().name("subject").value(iTestContext.getSuite().getName());
+                String customData = FileUtils.readFileToString(new File(htmlFile), StandardCharsets.UTF_8);
+                String replaceWidth = customData.replace("90%", "100%");
+                writer.name("customBody").value(replaceWidth);
+                writer.endObject().flush();*/
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/test/java/com/fh/unittests/testrail/TestReport.java
+++ src/test/java/com/fh/unittests/testrail/TestReport.java
@@ -1,6 +1,7 @@
 package com.fh.unittests.testrail;
 
 import com.fh.core.TestLocalController;
+import com.trigon.reports.TestRailReport;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
@@ -13,9 +14,12 @@ public class TestReport extends TestLocalController {
 
         author_ScenarioName("Nisha","Test Report");
         try {
-
-           addHeaderToCustomReport("ClassName","Passed","Failed","Skipped","TestRailLink");
-           addRowToCustomReport("OrderPlacemnet","C12344","C12346","C12343","https://www.testrail.com");
+            TestRailReport r = new TestRailReport();
+            r.initTestRailReport();
+            r.addRowToTestRailReport("OrderPlacemnet","C12344, C12345, C12346, C12346 , C12346, C12346, C12346","C12346,C12343","C12343, C12345");
+            //r.addRowToTestRailReport("","C12344","","C12343","https://www.testrail.com");
+            //r.addRowToTestRailReport("","C12344","","","https://www.testrail.com");
+            r.addRowToTestRailReport("Settings","C12344","C12346","C12343");
 
         }
         catch (Exception e)
