bc287b16d0dbd3d856e699ca5d90cda7318d6280
fh-vikram <vikram.s@foodhub.com>
2023-01-10 23:25:58 +0530

    TestRail Logic Implementation - Data provider testing classfile

---
+++ src/test/java/com/fh/unittests/testrail/TestRailDataProvider.java
@@ -0,0 +1,35 @@
+package com.fh.unittests.testrail;
+
+import com.fh.core.TestLocalController;
+import com.trigon.annotations.ExcelSheet;
+import com.trigon.dataprovider.DataProviders;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestRailDataProvider extends TestLocalController {
+    @Test(dataProvider = "getDataFromJson", dataProviderClass = DataProviders.class, groups = {"Sanity", "Regression"})
+    @ExcelSheet(name = "TR")
+    public void methodNameDemo() {
+        ArrayList<String> tcIDs=new ArrayList<>(Arrays.asList("C0001","C0002","C0003"));
+        author_ScenarioName("Vikram","Demo");
+        try {
+            logStepAction("1st TC", "C0001");
+
+            logReport("PASS", "1st step");
+            logReport("PASS", "2nd step");
+
+            logStepAction("2nd TC", "C0002,C0003");
+
+            logReport("PASS", "3rd step");
+            logReport("PASS", "step");
+        }
+        catch (Exception e)
+        {}
+        finally {
+            testTearDown(tcIDs);
+        }
+    }
+
+}
